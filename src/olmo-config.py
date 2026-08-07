fb621d3020988e12c77ef64bf4061c9ef754d82c
Trisha Salas <trisha@trishasalas.com>
2026-08-07 09:57:52 -0500

    olmo file naming cleanup

---
+++ src/olmo-config.py
@@ -0,0 +1,21 @@
+# use the following code to grab the correct revision and store as constants.
+
+# import re
+# LAST_STAGE1 = lambda names: max(
+#    (b for b in names if b.startswith("stage1")),
+#    key=lambda b: int(re.search(r"step(\d+)", b).group(1)),
+#)
+
+
+OLMO_REVISIONS = {
+    "allenai/OLMo-2-0425-1B": "stage1-step1907359-tokens4001B",
+    "allenai/OLMo-2-1124-7B": "stage1-step928646-tokens3896B",
+    "allenai/OLMo-2-1124-13B": "stage1-step596057-tokens5001B",
+}
+
+
+# Call in every OLMo Notebook
+
+# from src.olmo_config import OLMO_REVISIONS
+# revision = OLMO_REVISIONS[model_id]
+
